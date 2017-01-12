82569d9ee11a4f7c6ef3a16825569590ff939c57
NewYork <newyork@NewYork.local>
2017-01-12 13:42:15 +0900

    commit 17012

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,29 @@
+function $id(id) { return document.getElementById(id); }
+function $class(c) { return document.getElementsByClassName(c); }
+
+/*example
+var elements = $id('name');
+var elements = $class('name').item(0);
+*/
+
+var hasClass = function (elem, className) { return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');}
+var addClass = function (elem, className) { if (!hasClass(elem, className)) { elem.className += ' ' + className;}}
+var removeClass = function (elem, className) { var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' '; if (hasClass(elem, className)) {while (newClass.indexOf(' ' + className + ' ') >= 0 ) { newClass = newClass.replace(' ' + className + ' ', ' '); } elem.className = newClass.replace(/^\s+|\s+$/g, '');}}
+
+/*example
+addClass($id('sample'), 'sampleStyle');
+removeClass($id('sample'), 'sampleStyle');
+*/
+
+// window.addEventListener("keyup", function(event) {
+//     event.preventDefault();
+//     if (event.keyCode == 13) {
+//         document.getElementById("button").click();
+//     }
+// });
+
+// window.scrollTo( 0, 0 ) ;
+
 $(function(){
 	$('a[href^=#]').click(function(){
 		var speed = 500;
@@ -7,6 +33,38 @@ $(function(){
 		$("html, body").animate({scrollTop:position}, speed, "swing");
 		return false;
 	});
+
+	// $("button").click(function(){
+	// });
+
+// $(window).scroll(function () {
+//        if($(window).scrollTop() >= scrollPosC) {
+//        } else {
+//        }
+//    });
+
+// Waypoint.refreshAll();
+
+// $('.js-p2').waypoint({
+//         handler: function() {
+//         	addClass(about, 'about_m');
+			// this.destroy();
+//         },
+//         offset: '70%'
+// });
+
+// $(".works_box").hover(
+// 	  function () {
+// 	    $(this).find(".caption").addClass("works_box_on");
+// 	    $(this).find(".caption").removeClass("works_box_of");
+// 	  },
+// 	  function () {
+// 	    $(this).find(".caption").addClass("works_box_of");
+// 	    $(this).find(".caption").removeClass("works_box_on");
+// 	  }
+// );
+
+
 });
 
 /*hoge*/
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var uglify = require("gulp-uglify");
  
 gulp.task("sass", function() {
     gulp.src("assets/sass/**/*.scss")
@@ -8,6 +10,25 @@ gulp.task("sass", function() {
         .pipe(gulp.dest("assets/css"));
 });
 
+gulp.task("js", function() {
+    gulp.src(["assets/js/**/*.js"])
+        .pipe(uglify())
+        .pipe(gulp.dest("assets/jsmin"));
+});
+
 gulp.task("default", function() {
     gulp.watch("assets/sass/**/*.scss",["sass"]);
+    gulp.watch("assets/js/**/*.js",["js"]);
 });
+
+/*
+node -v
+nvm ls
+nvm install 0.11
+nvm use 0.11
+
+npm install gulp --save-dev
+npm install gulp-sass --save-dev
+npm install gulp-autoprefixer --save-dev
+npm install gulp-uglify --save-dev
+*/
