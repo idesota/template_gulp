d2600450a4c1638ab7382b29833805740652cc58
NewYork <newyork@NewYork.local>
2017-01-27 11:48:07 +0900

    mod script2

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,1 +1,25 @@
-console.log("script2");
+var $body,
+    $items,
+    _itemList = {};
+
+$body = document.body;
+$items = $body.children;
+
+for(var i=0;i<$items.length;i++){
+  //console.log($items[i].classList[0]);
+  var key = $items[i].classList[0];
+
+  _itemList[key] = $items[i];
+}
+
+console.log(_itemList);
+
+// クラスを追加する場合
+_itemList['クラス名'].classList.add('追加したいクラス');
+// 削除する場合
+_itemList['クラス名'].classList.remove('削除したいクラス');
+
+// jQ
+$(function(){
+
+});
